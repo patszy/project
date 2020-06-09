a80df21286e7d5e38b0c296fb838e8273d63f315
patszy <patszy97@interia.pl>
2020-06-09 14:56:13 +0200

    Created responsive menu.

--- js/events.js
+++ js/events.js
@@ -1,9 +1,17 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener(`DOMContentLoaded`, () => {
     const closeBtn = document.querySelectorAll(`.user__container .container__close`);
+    const hamburgerMenu = document.querySelector(`.hamburger`);
 
-    for(let btn of closeBtn) {
-        btn.addEventListener('click', (event) => {
-            event.target.closest(`.user__container`).classList.toggle('user__container--show');
-        });
+    const handleHamburgerClick = (element) => {
+        element.classList.toggle(`hamburger--active`);
+        document.querySelector(`.main__nav`).classList.toggle(`main__nav--active`);
     }
+
+    const handleCloseClick = (element) => {
+        element.classList.toggle(`user__container--show`);
+    }
+
+    hamburgerMenu.addEventListener(`click`, () => handleHamburgerClick(hamburgerMenu));
+
+    for(let btn of closeBtn) btn.addEventListener(`click`, () => handleCloseClick(btn.closest(`.user__container`)));
 });
