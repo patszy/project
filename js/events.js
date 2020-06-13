3a4212cae3d53abbadad2715dc6a10743b11f067
patszy <patszy97@interia.pl>
2020-06-13 21:26:43 +0200

    Added login form

--- js/events.js
+++ js/events.js
@@ -2,6 +2,8 @@ document.addEventListener(`DOMContentLoaded`, () => {
     const closeBtn = document.querySelectorAll(`.user__container .container__close`);
     const hamburgerMenu = document.querySelector(`.hamburger`);
 
+    const loginButton = document.querySelector(`.login__button`);
+
     const handleHamburgerClick = (element) => {
         element.classList.toggle(`hamburger--active`);
         document.querySelector(`.main__nav`).classList.toggle(`main__nav--active`);
@@ -11,7 +13,13 @@ document.addEventListener(`DOMContentLoaded`, () => {
         element.classList.toggle(`user__container--show`);
     }
 
-    hamburgerMenu.addEventListener(`click`, () => handleHamburgerClick(hamburgerMenu));
+    const handleLoginClick = (element) => {
+        element.nextElementSibling.classList.toggle(`form__login--show`);
+    }
 
+    loginButton.addEventListener('click', () => {
+        handleLoginClick(loginButton);
+    });
+    hamburgerMenu.addEventListener(`click`, () => handleHamburgerClick(hamburgerMenu));
     for(let btn of closeBtn) btn.addEventListener(`click`, () => handleCloseClick(btn.closest(`.user__container`)));
 });
